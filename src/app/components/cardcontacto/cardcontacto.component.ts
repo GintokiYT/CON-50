0804c4eeada484be61d875488ee75e60f30794fd
Renato Navarro <devrenatonavarro@gmail.com>
2023-01-08 19:13:45 -0500

    modal chiquito

--- src/app/components/cardcontacto/cardcontacto.component.ts
+++ src/app/components/cardcontacto/cardcontacto.component.ts
@@ -1,11 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Contacto {
+  imagen: string;
+  nombre: string;
+  celular: string;
+  correo: string;
+  etiquetas: Etiqueta[];
+}
+
+interface Etiqueta {
+  nombre: string;
+  tipo: string;
+}
+
 @Component({
   selector: 'app-cardcontacto',
   templateUrl: './cardcontacto.component.html',
   styleUrls: ['./cardcontacto.component.scss'],
 })
 export class CardcontactoComponent implements OnInit {
+
+  contacto: Contacto = {
+    imagen: '/assets/contacto.png',
+    nombre: 'Carla Mamani',
+    celular: '+51 943 786 708',
+    correo: 'carla@gmail.com',
+    etiquetas: [
+      {
+        nombre: 'Cliente interno',
+        tipo: 'etiqueta-azul'
+      },
+      {
+        nombre: 'Lead',
+        tipo: 'etiqueta-roja'
+      }
+    ]
+  }
 
   constructor() { }
 
--- src/app/components/empresasyventas/empresasyventas.component.ts
+++ src/app/components/empresasyventas/empresasyventas.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Empresa {
+  imagen: string;
+  nombre: string;
+  valor: string;
+}
+
 @Component({
   selector: 'app-empresasyventas',
   templateUrl: './empresasyventas.component.html',
@@ -7,8 +13,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpresasyventasComponent implements OnInit {
 
+  empresas: Empresa[] = [
+    {
+      imagen: '/assets/empresas/elishop.svg',
+      nombre: 'ELISHOP',
+      valor: 'S/3.8K'
+    },
+    {
+      imagen: '/assets/empresas/empresa.svg',
+      nombre: 'GEOR',
+      valor: 'S/1.4K'
+    },
+    {
+      imagen: '/assets/empresas/tesla.svg',
+      nombre: 'Ortiz Mayorista',
+      valor: 'S/12.7K'
+    },
+    {
+      imagen: '/assets/empresas/empresa.svg',
+      nombre: 'High Studio',
+      valor: 'S/10.1K'
+    },
+    {
+      imagen: '/assets/empresas/empresa.svg',
+      nombre: 'Corporación Reque',
+      valor: 'S/890'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {}
+
+  onClick() {
+    console.log('Diste click');
+  }
 
 }
